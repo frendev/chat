575361ce07b6c6eaa1da629d9cd32438b60c9f42
frendev <51207723+frendev@users.noreply.github.com>
2019-10-21 19:54:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express=require('express')
 const socket=require('socket.io')
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://chat-ebea6.firebaseio.com"
+});
+const db=admin.firestore()
 
 const app=express()
 
@@ -16,9 +25,23 @@ const io=socket(server)
 //on incoming connection    
 io.on('connection',(socket)=>{
     console.log('socket connected');
+    //geting chats from database
+    let chats=db.collection('event-chatroom')
+    chats.get().then(snapshot=>{
+        snapshot.docs.forEach(doc=>{
+            socket.emit('prev_chats',doc.data())
+            // console.log(doc.data())
+        })
+    })
+    . catch(err=>console.log(err))
 
     //sending the chat to all connected users
     socket.on('send_chat',(data)=>{
+        chats.add({
+            handle:data.chat_handle,
+            message:data.chat_message,
+            timestamp:data.chat_timestamp
+        })
         io.sockets.emit('rec_chat',data)
     })
 
